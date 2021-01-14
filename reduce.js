d9abbaf5ce99a8a3acdca792b8ae8fb212a330f3
Mostafa Nassr <mostafa.nassr88@gmail.com>
2021-01-14 12:59:58 -0500

    added js reduce logic

--- reduce.js
+++ reduce.js
@@ -6,7 +6,11 @@ Examples:
     extractValue(arr,'name') // ['Elie', 'Tim', 'Matt', 'Colt']
 */
 
-function extractValue(arr, key) {}
+function extractValue(arr, key) {
+    return arr.reduce((newArr, nextObj) => {
+        return newArr.concat(nextObj[key]);
+    }, []);
+}
 
 /*
 Write a function called vowelCount which accepts a string and returns an object with the keys as the vowel and the values as the number of times the vowel appears in the string. This function should be case insensitive so a lowercase letter and uppercase letter should count
@@ -19,7 +23,14 @@ Examples:
     vowelCount('I Am awesome and so are you') // {i: 1, a: 4, e: 3, o: 3, u: 1};
 */
 
-function vowelCount(str) {}
+function vowelCount(str) {
+    const vowels = 'aeiou';
+    const strArrVowels = str.toLowerCase().split('').filter(letter => vowels.indexOf(letter) !== -1);
+    return strArrVowels.reduce((vowelObj, nextLetter) => {
+        vowelObj[nextLetter] ? vowelObj[nextLetter] += 1 : vowelObj[nextLetter] = 1;
+        return vowelObj;
+    }, {});
+}
 
 /*
 Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it with each object now including the key and value passed to the function.
@@ -36,7 +47,12 @@ Examples:
        ]
 */
 
-function addKeyAndValue(arr, key, value) {}
+function addKeyAndValue(arr, key, value) {
+    return arr.reduce((newArr, nextObj, indxOfObj) => {
+        newArr[indxOfObj][key] = value;
+        return newArr;
+    }, arr);
+}
 
 /*
 Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray. 
@@ -60,4 +76,9 @@ Examples:
     partition(names, isLongerThanThreeCharacters) // [['Elie', 'Colt', 'Matt'], ['Tim']]
 */
 
-function partition(arr, callback) {}
+function partition(arr, callback) {
+    return arr.reduce((acc, next, i) => {
+        callback(arr[i]) ? acc[0].push(next) : acc[1].push(next);
+        return acc;
+    }, [[],[]]);
+}
